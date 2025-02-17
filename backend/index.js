57cc921ecac36aee9de5d3445b69326c74b0cde1
mahilmithranks <mahilmithranks2007@gmail.com>
2025-02-17 15:19:56 +0530

    done

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/ping', (req, res) => {
+  res.send('Pong! - College Myths Edition');
+});
+
+app.get('/myths', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'myths.html'));
+});
+
+app.get('/promises', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'promises.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port} - Debunking College Myths`);
+});
